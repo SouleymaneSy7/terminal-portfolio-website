880f83720b9e4c16a59d01866a013890a0173419
souleymanesy7 <souleymanesyservices@gmail.com>
2025-03-19 20:19:25 +0000

    feat: add the local font families

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,61 @@
 import type { Metadata } from "next";
+import localFont from "next/font/local";
+
 import "./globals.css";
+
+const JetbrainsMono = localFont({
+  src: [
+    {
+      path: "/public/fonts/jetbrains-mono-regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "/public/fonts/jetbrains-mono-semi-bold.woff2",
+      weight: "600",
+      style: "normal",
+    },
+  ],
+  variable: "--font-jetbrains-mono",
+  display: "swap",
+  fallback: ["monospace"],
+});
+
+const CascadiaCode = localFont({
+  src: [
+    {
+      path: "/public/fonts/cascadia-code-regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "/public/fonts/cascadia-code-semi-bold.woff2",
+      weight: "600",
+      style: "normal",
+    },
+  ],
+  variable: "--font-cascadia-code",
+  display: "swap",
+  fallback: ["monospace"],
+});
+
+const FiraCode = localFont({
+  src: [
+    {
+      path: "/public/fonts/fira-code-regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "/public/fonts/fira-code-semi-bold.woff2",
+      weight: "600",
+      style: "normal",
+    },
+  ],
+  variable: "--font-fira-code",
+  display: "swap",
+  fallback: ["monospace"],
+});
 
 export const metadata: Metadata = {
   title: "Souleymane Sy | terminal portfolio",
@@ -14,7 +70,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`antialiased`}>{children}</body>
+      <body
+        className={`${JetbrainsMono.variable} ${FiraCode.variable} ${CascadiaCode.variable} antialiased`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
