40148c4d0ada968f0d94a0f15962dc4ffe8bb3aa
souleymanesy7 <souleymanesyservices@gmail.com>
2025-03-19 16:16:09 +0000

    chore: add the command output constants

---
+++ src/constants/index.ts
@@ -0,0 +1,43 @@
+export const helpCommandOutput = [
+  "Here are all the available commands :",
+  "",
+  "about     - About me.",
+  "date      - Show the current date.",
+  "exit      - Close the windows.",
+  "hostname  - Display the host name.",
+  "help      - Show available command list",
+  "projects  - Show projects list.",
+  "theme     - Change the themes.",
+  "neofetch  - Show User Personal informations.",
+  "sudo      - An Easter eggs for the Admin.",
+  "whoami    - Display the guest name.",
+  "welcome   - Display the welcome banner.",
+];
+
+export const aboutMeCommandOutput = ["About Me..."];
+
+export const dateCommandOutput = [new Date().toString()];
+
+export const hostNameCommandOutput = ["souleymane-sy-portfolio"];
+
+export const whoAmICommandOutput = ["guest"];
+
+export const exitCommandOutput = ["Please close the tab to exit."];
+
+export const projectsCommandOutput = ["List of Projects"];
+
+export const themeCommandOutput = [
+  "Change the theme. Features comming soon...",
+];
+
+export const neofetchCommandOutput = ["User information"];
+
+export const sudoCommandOutput = [
+  "You're not the admin of this website;",
+  "Please contact the admin.",
+];
+
+export const welcomeCommandOutput = [
+  "Welcome on my terminal portfolio.",
+  "Type 'help' to see available commands.",
+];
