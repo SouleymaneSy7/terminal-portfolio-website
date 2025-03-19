20992f5454c537edd1935ece2958457f3bc2578d
souleymanesy7 <souleymanesyservices@gmail.com>
2025-03-19 22:04:40 +0000

    style: add component styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ import Terminal from "@/components/Terminal";
 
 export default function Home() {
   return (
-    <main className="w-full h-screen">
-      <div>
+    <main className="w-full h-screen overflow-hidden p-4 lg:p-5">
+      <div className="terminal-container | w-full h-full overflow-y-scroll border-2 border-[var(--primary-clr)] rounded-md bg-[var(--foreground-clr)] p-2 lg:p-4">
         <Terminal />
       </div>
     </main>
