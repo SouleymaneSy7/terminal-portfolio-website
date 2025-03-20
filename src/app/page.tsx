c2613b0eef2923a4aae2f0302341c06dcba6b8e7
souleymanesy7 <souleymanesyservices@gmail.com>
2025-03-20 21:18:24 +0000

    feat: update the homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,15 @@ import * as React from "react";
 import Terminal from "@/components/Terminal";
 
 export default function Home() {
+  const containerRef = React.useRef(null);
+
   return (
     <main className="w-full h-screen overflow-hidden p-4 lg:p-5">
-      <div className="terminal-container | w-full h-full overflow-y-scroll border-2 border-[var(--primary-clr)] rounded-md bg-[var(--foreground-clr)] p-2 lg:p-4">
-        <Terminal />
+      <div
+        className="terminal-container | w-full h-full overflow-y-scroll border-2 border-[var(--primary-clr)] rounded-md bg-[var(--foreground-clr)] p-2 lg:p-4"
+        ref={containerRef}
+      >
+        <Terminal containerRef={containerRef} />
       </div>
     </main>
   );
