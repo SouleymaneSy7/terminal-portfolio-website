0efc285c7373381a3e94496b78db9558229c9677
souleymanesy7 <souleymanesyservices@gmail.com>
2025-03-23 15:57:23 +0000

    feat: update the commandOutput component

--- src/components/CommandOutput.tsx
+++ src/components/CommandOutput.tsx
@@ -9,7 +9,13 @@ const CommandOutput: React.FC<CommandOutputPropsType> = ({
   containerRef,
   setAnimationIsComplete,
 }) => {
-  const [displayText, setDisplayText] = React.useState<string[]>([]);
+  const [displayText, setDisplayText] = React.useState<
+    {
+      id: string;
+      type: "text" | "link" | "html";
+      content: string[];
+    }[]
+  >([]);
   const [currentIndex, setCurrrentIndex] = React.useState(0);
   const [isComplete, setIsComplete] = React.useState(false);
 
@@ -23,7 +29,7 @@ const CommandOutput: React.FC<CommandOutputPropsType> = ({
     if (containerRef && containerRef.current) {
       containerRef.current.scrollTo(0, containerRef.current.scrollHeight);
     }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [displayText]);
 
   React.useEffect(() => {
@@ -31,9 +37,15 @@ const CommandOutput: React.FC<CommandOutputPropsType> = ({
     setAnimationIsComplete(false);
 
     if (currentIndex < outputLines.length) {
-      const currentWord = outputLines[currentIndex];
+      const currentId = outputLines[currentIndex].id;
+      const currentType = outputLines[currentIndex].type;
+      const currentWord = outputLines[currentIndex].content;
+
       const timeout = setTimeout(() => {
-        setDisplayText((prevState) => [...prevState, currentWord]);
+        setDisplayText((prevState) => [
+          ...prevState,
+          { id: currentId, type: currentType, content: currentWord },
+        ]);
         setCurrrentIndex((prevState) => prevState + 1);
       }, speed);
 
@@ -55,11 +67,33 @@ const CommandOutput: React.FC<CommandOutputPropsType> = ({
 
   return (
     <div>
-      {displayText.map((output: string, index: number) => (
-        <React.Fragment key={index}>
-          <p>{output}</p>
-        </React.Fragment>
-      ))}
+      {displayText.map((item) => {
+        if (item.type === "text") {
+          return (
+            <div key={item.id} className="terminal-ouput">
+              {item.content.map((text, index) => {
+                return <p key={index}>{text}</p>;
+              })}
+            </div>
+          );
+        } else if (item.type === "html") {
+          return (
+            <div
+              key={item.id}
+              className="terminal-ouput"
+              dangerouslySetInnerHTML={{ __html: item.content }}
+            />
+          );
+        } else if (item.type === "link") {
+          return (
+            <div className="terminal-ouput" key={item.id}>
+              <a href={item.content[0]} target="_blank" rel="noreferrer">
+                {item.content[1]}
+              </a>
+            </div>
+          );
+        }
+      })}
     </div>
   );
 };
