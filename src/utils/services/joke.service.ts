ab5586f66321e1b6bd2753d7091cc7261527d4ff
SouleymaneSy7 <souleymanesyservices@gmail.com>
2025-09-30 23:02:33 +0000

    feat(joke service): add the joke fetch service

---
+++ src/utils/services/joke.service.ts
@@ -0,0 +1,26 @@
+import { JokeResponseType } from "@/types";
+import axios from "axios";
+
+const JOKE_API_URL = "https://v2.jokeapi.dev/joke";
+
+const jokeApi = axios.create({
+  baseURL: JOKE_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  responseType: "json",
+});
+
+export const jokeService = {
+  getRandomJoke: async (): Promise<JokeResponseType | undefined> => {
+    try {
+      const response = await jokeApi.get<JokeResponseType | undefined>(
+        `/Programming?type=twopart`,
+      );
+      const data = response.data;
+      return data;
+    } catch (error) {
+      console.error("Error Fetching Joke: ", error);
+    }
+  },
+};
