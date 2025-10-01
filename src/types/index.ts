cf8f735ea41461dcfc4a53844714e3c358f4caed
SouleymaneSy7 <souleymanesyservices@gmail.com>
2025-10-01 01:55:58 +0000

    chore(types): update the quote-response types

--- src/types/index.ts
+++ src/types/index.ts
@@ -61,16 +61,11 @@ export interface JokeResponseType {
   lang: string;
 }
 
-export type QuoteResponseType = Array<{
-  _id: string;
-  // The quotation text
-  content: string;
-  // The full name of the author
-  author: string;
-  // The `slug` of the quote author
-  authorSlug: string;
-  // The length of quote (number of characters)
-  length: number;
-  // An array of tag names for this quote
-  tags: string[];
-}>;
+export interface AdviceSlip {
+  id: number;
+  advice: string;
+}
+
+export interface QuoteResponseType {
+  slip: AdviceSlip;
+}
