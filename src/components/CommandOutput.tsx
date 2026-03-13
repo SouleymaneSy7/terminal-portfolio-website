968b15bac6e83945f4448498b6b516a22306dccb
SouleymaneSy7 <souleymanesyservices@gmail.com>
2026-03-13 01:20:52 +0000

    fix: fix the DOMPurify issue & the animation speed issue for long text

--- src/components/CommandOutput.tsx
+++ src/components/CommandOutput.tsx
@@ -1,10 +1,27 @@
 "use client";
 
 import * as React from "react";
-
 import DOMPurify from "dompurify";
-
 import { CommandOutputPropsType } from "@/types";
+
+if (typeof window !== "undefined") {
+  DOMPurify.addHook("afterSanitizeAttributes", function (node) {
+    if ("target" in node) {
+      node.setAttribute("target", "_blank");
+    }
+
+    if ("rel" in node) {
+      node.setAttribute("rel", "noopener noreferrer");
+    }
+
+    if (
+      !node.hasAttribute("target") &&
+      (node.hasAttribute("xlink:href") || node.hasAttribute("href"))
+    ) {
+      node.setAttribute("xlink:show", "new");
+    }
+  });
+}
 
 const CommandOutput: React.FC<CommandOutputPropsType> = ({
   speed,
@@ -13,13 +30,22 @@ const CommandOutput: React.FC<CommandOutputPropsType> = ({
   containerRef,
   setAnimationIsComplete,
 }) => {
-  const [displayText, setDisplayText] = React.useState<string[]>([]);
-  const [currentIndex, setCurrrentIndex] = React.useState(0);
+  const [displayText, setDisplayText] = React.useState<string[] | string[][]>(
+    [],
+  );
+  const [currentIndex, setCurrentIndex] = React.useState(0);
   const [isComplete, setIsComplete] = React.useState(false);
+
+  const effectiveSpeed = React.useMemo(() => {
+    if (speed !== undefined) return speed;
+    const lineCount = outputLines.length;
+    if (lineCount > 30) return 20;
+    if (lineCount > 15) return 40;
+    return 100;
+  }, [speed, outputLines.length]);
 
   const handleComplete = React.useCallback(() => {
     setIsComplete(true);
-
     setAnimationIsComplete(true);
   }, [setAnimationIsComplete]);
 
@@ -27,6 +53,7 @@ const CommandOutput: React.FC<CommandOutputPropsType> = ({
     if (containerRef && containerRef.current) {
       containerRef.current.scrollTo(0, containerRef.current.scrollHeight);
     }
+
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [displayText]);
 
@@ -40,23 +67,23 @@ const CommandOutput: React.FC<CommandOutputPropsType> = ({
     setAnimationIsComplete(false);
 
     if (currentIndex < outputLines.length) {
-      const currentWord = outputLines[currentIndex];
+      const currentLine = outputLines[currentIndex];
 
       const timeout = setTimeout(() => {
-        setDisplayText((prevState) => [...prevState, currentWord]);
-        setCurrrentIndex((prevState) => prevState + 1);
-      }, speed);
+        setDisplayText(
+          (prev) => [...prev, currentLine] as string[] | string[][],
+        );
+        setCurrentIndex((prev) => prev + 1);
+      }, effectiveSpeed);
 
-      return () => {
-        clearTimeout(timeout);
-      };
+      return () => clearTimeout(timeout);
     } else {
       handleComplete();
     }
   }, [
     displayText,
     currentIndex,
-    speed,
+    effectiveSpeed,
     outputLines,
     isComplete,
     handleComplete,
@@ -66,40 +93,19 @@ const CommandOutput: React.FC<CommandOutputPropsType> = ({
   if (outputTypes === "text") {
     return (
       <React.Fragment>
-        {displayText.map((text, index) => {
-          return (
-            <div key={index} className="terminal-output  whitespace-pre">
-              <p key={index}>{text}</p>
-            </div>
-          );
-        })}
+        {(displayText as string[]).map((text, index) => (
+          <div key={index} className="terminal-output whitespace-pre">
+            <p>{text}</p>
+          </div>
+        ))}
       </React.Fragment>
     );
-  } else if (outputTypes === "html") {
+  }
+
+  if (outputTypes === "html") {
     return (
       <React.Fragment>
-        {displayText.map((text, index) => {
-          DOMPurify.addHook("afterSanitizeAttributes", function (node) {
-            if ("target" in node) {
-              node.setAttribute("target", "_blank");
-            }
-
-            if ("rel" in node && node.getAttribute("rel") !== "noreferrer") {
-              node.setAttribute("rel", "noopener noreferrer");
-            }
-
-            if ("rel" in node) {
-              node.setAttribute("rel", "noreferrer");
-            }
-
-            if (
-              !node.hasAttribute("target") &&
-              (node.hasAttribute("xlink:href") || node.hasAttribute("href"))
-            ) {
-              node.setAttribute("xlink:show", "new");
-            }
-          });
-
+        {(displayText as string[]).map((text, index) => {
           const cleanText = DOMPurify.sanitize(text, {
             USE_PROFILES: { html: true },
           });
@@ -107,25 +113,25 @@ const CommandOutput: React.FC<CommandOutputPropsType> = ({
           return (
             <div
               key={index}
-              className="terminal-ouput space-y-8"
+              className="terminal-output space-y-8"
               dangerouslySetInnerHTML={{ __html: cleanText }}
             />
           );
         })}
       </React.Fragment>
     );
-  } else if (outputTypes === "link") {
+  }
+
+  if (outputTypes === "link") {
     return (
       <React.Fragment>
-        {displayText.map((text, index) => {
-          return (
-            <div className="terminal-ouput" key={index}>
-              <a href={text[0]} target="_blank" rel="noreferrer">
-                {text[1]}
-              </a>
-            </div>
-          );
-        })}
+        {(displayText as string[][]).map((text, index) => (
+          <div className="terminal-output" key={index}>
+            <a href={text[0]} target="_blank" rel="noreferrer">
+              {text[1]}
+            </a>
+          </div>
+        ))}
       </React.Fragment>
     );
   }
