d5c9bc66ef870009bd511638aadc6950ecfd9390
SouleymaneSy7 <souleymanesyservices@gmail.com>
2026-03-13 16:18:46 +0000

    chore(export): add the loader export file

---
+++ src/components/ui/loaders/index.ts
@@ -0,0 +1,5 @@
+export { default as AsciiProgressBar } from "./ascii-progress-bar";
+export { default as RotatingSpinner } from "./rotating-spinner";
+export { default as TypewriterCursor } from "./typewritter-cursor";
+export { default as DotsAccumulator } from "./dots-accumulator";
+export { default as BrailleSpinner } from "./braille-spinner";
