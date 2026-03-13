8c9b135c7b9e79ea7f40601279f5e4d233831e21
SouleymaneSy7 <souleymanesyservices@gmail.com>
2026-03-13 01:11:26 +0000

    fix: fix the getDate & getTime issues

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,32 +1,30 @@
-const now = new Date();
-
-const year = now.getFullYear();
-const month = now.getMonth() + 1;
-const day = now.getDate();
-
-const getDate = () => {
-  return `Current Date: ${year}-${month}-${day}`;
-};
-
-const getTime = () => {
-  return `Current Time: ${formatTimeToAMPM(now)}`;
-};
-
 // Function to format the time to AM/PM
-function formatTimeToAMPM(date: Date) {
+function formatTimeToAMPM(date: Date): string {
   let hours = date.getHours();
   let minutes: string | number = date.getMinutes();
 
   const ampm = hours >= 12 ? "PM" : "AM";
 
   hours = hours % 12;
-
   hours = hours ? hours : 12;
   minutes = minutes < 10 ? "0" + minutes : minutes;
 
-  const time = hours + ":" + minutes + " " + ampm;
-
-  return time;
+  return hours + ":" + minutes + " " + ampm;
 }
+
+const getDate = (): string => {
+  const now = new Date();
+  const year = now.getFullYear();
+  const month = now.getMonth() + 1;
+  const day = now.getDate();
+
+  return `Current Date: ${year}-${month}-${day}`;
+};
+
+const getTime = (): string => {
+  const now = new Date();
+
+  return `Current Time: ${formatTimeToAMPM(now)}`;
+};
 
 export { getDate, getTime };
