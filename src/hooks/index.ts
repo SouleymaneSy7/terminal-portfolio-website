8e838a25a61296133586c6b3fc1010c769ac029c
SouleymaneSy7 <souleymanesyservices@gmail.com>
2026-04-29 16:31:39 +0000

    feat: add barrel file for all custom hooks

---
+++ src/hooks/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Hooks barrel — single import point.
+ *
+ */
+
+export * from "./useCommandHistory";
+export * from "./useDebounce";
+export * from "./useDynamicFont";
+export * from "./useLocalStorage";
+export * from "./useReverseSearch";
+export * from "./useSuggestions";
+export * from "./useThemeFont";
+
