c9fbf479b4bc38607ce3d272c4e673a2f01c2fda
SouleymaneSy7 <souleymanesyservices@gmail.com>
2026-04-29 16:02:17 +0000

    feat(a11y): add VisuallyHidden component for screen readers

---
+++ src/components/common/VisuallyHidden.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import * as React from "react";
+import { VisuallyHiddenPropsType } from "@/types";
+
+const VisualyHidden: React.FC<VisuallyHiddenPropsType> = ({
+  children,
+  ...delegatedProps
+}) => {
+  const [forceShow, setForceShow] = React.useState<boolean>(false);
+
+  React.useEffect(() => {
+    if (process.env.NODE_ENV !== "production") {
+      const handleKeyDown = (event: KeyboardEvent) => {
+        if (event.key === "Alt") {
+          setForceShow(true);
+        }
+      };
+
+      const handleKeyUp = (event: KeyboardEvent) => {
+        if (event.key === "Alt") {
+          setForceShow(false);
+        }
+      };
+
+      window.addEventListener("keydown", handleKeyDown);
+      window.addEventListener("keyup", handleKeyUp);
+
+      return () => {
+        window.removeEventListener("keydown", handleKeyDown);
+        window.removeEventListener("keyup", handleKeyUp);
+      };
+    }
+  }, []);
+
+  if (forceShow) {
+    return <React.Fragment>{children}</React.Fragment>;
+  }
+
+  return (
+    <span className="visually-hidden" {...delegatedProps}>
+      {children}
+    </span>
+  );
+};
+
+export default VisualyHidden;
