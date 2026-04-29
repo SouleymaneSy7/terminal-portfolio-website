0b3d756fd5a1f08354322d690f83f8bf8bcceb22
SouleymaneSy7 <souleymanesyservices@gmail.com>
2026-04-29 16:19:54 +0000

    refactor: update loader exports with new names

--- src/components/ui/loaders/index.ts
+++ src/components/ui/loaders/index.ts
@@ -1,5 +1,6 @@
-export { default as AsciiProgressBar } from "./ascii-progress-bar";
-export { default as RotatingSpinner } from "./rotating-spinner";
-export { default as TypewriterCursor } from "./typewritter-cursor";
-export { default as DotsAccumulator } from "./dots-accumulator";
-export { default as BrailleSpinner } from "./braille-spinner";
+export { default as AsciiProgressBar } from "./AsciiProgressBar";
+export { default as BrailleSpinner } from "./BrailleSpinner";
+export { default as DotsAccumulator } from "./DotsAccumulator";
+export { default as LoadingIndicator } from "./LoadingIndicator";
+export { default as RotatingSpinner } from "./RotatingSpinner";
+export { default as TypewriterCursor } from "./TypewriterCursor";
