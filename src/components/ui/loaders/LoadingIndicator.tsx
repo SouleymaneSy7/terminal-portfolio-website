d6d6d4e64974da62590c3d7f71db991dce4bfe68
SouleymaneSy7 <souleymanesyservices@gmail.com>
2026-04-29 16:19:01 +0000

    refactor: move LoadingIndicator into loaders/ subdirectory

---
+++ src/components/ui/loaders/LoadingIndicator.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import { LoadingIndicatorPropsType } from "@/types";
+import * as React from "react";
+import AsciiProgressBar from "./AsciiProgressBar";
+import BrailleSpinner from "./BrailleSpinner";
+import DotsAccumulator from "./DotsAccumulator";
+import RotatingSpinner from "./RotatingSpinner";
+import TypewriterCursor from "./TypewriterCursor";
+
+const LoadingIndicator: React.FC<LoadingIndicatorPropsType> = ({
+  variant = "braille",
+  label,
+}) => {
+  switch (variant) {
+    case "ascii":
+      return <AsciiProgressBar label={label} />;
+
+    case "spinner":
+      return <RotatingSpinner label={label} />;
+
+    case "typewriter":
+      return <TypewriterCursor label={label} />;
+
+    case "dots":
+      return <DotsAccumulator label={label} />;
+
+    case "braille":
+      return <BrailleSpinner label={label} />;
+
+    default:
+      return <BrailleSpinner label={label} />;
+  }
+};
+
+export default LoadingIndicator;
